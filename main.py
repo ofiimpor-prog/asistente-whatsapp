d6245d45e72b111d30f8b9674a6778ae6d8a8e9f
ofiimpor-prog <ofi.impor@gmail.com>
2026-02-11 13:15:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -77,3 +77,7 @@ def whatsapp():
 
 if __name__ == "__main__":
     app.run(port=10000)
+if __name__ == "__main__":
+    # Render asigna un puerto dinámico, lo capturamos aquí
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
